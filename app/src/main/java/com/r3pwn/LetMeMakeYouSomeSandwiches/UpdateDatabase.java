4c1ff8c7376491f50205b2be686e0c9460728a63
r3pwn <wickett06@gmail.com>
2016-06-17 21:35:20 -0400

    finally fix this whole thing up

--- app/src/main/java/com/r3pwn/LetMeMakeYouSomeSandwiches/UpdateDatabase.java
+++ app/src/main/java/com/r3pwn/LetMeMakeYouSomeSandwiches/UpdateDatabase.java
@@ -8,6 +8,8 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import eu.chainfire.libsuperuser.Shell;
+import java.io.*;
+import android.util.*;
 
 class UpdateDatabase implements View.OnClickListener {
     private ToggleButton toggleButton;
@@ -33,14 +35,49 @@ class UpdateDatabase implements View.OnClickListener {
         SharedPreferences.Editor editor = mainActivity.defaultSharedPreferences.edit();
 
         // Look at me. I'm the captain now.
-        Shell.SU.run("cp /data/data/com.google.android.gsf/databases/gservices.db /data/data/com.r3pwn.LetMeMakeYouSomeSandwiches/databases/gservices.db\n");
-        SQLiteDatabase db = mainActivity.openOrCreateDatabase("gservices.db", Context.MODE_WORLD_READABLE, null);
+		File databasesDir = new File("/data/data/com.r3pwn.LetMeMakeYouSomeSandwiches/databases");
+		File gservicesDb = new File("/data/data/com.r3pwn.LetMeMakeYouSomeSandwiches/databases/gservices.db");
+		File gservicesWorkingDb = new File("/data/data/com.r3pwn.LetMeMakeYouSomeSandwiches/databases/gservices_working.db");
+		
+		if (!databasesDir.exists())
+		{
+			databasesDir.mkdir();
+		}
+		
+		Shell.SU.run("cp /data/data/com.google.android.gsf/databases/gservices.db " + gservicesDb + "\n");
+		Shell.SU.run("restorecon " + gservicesDb);
+		Shell.SU.run("chmod 0777 " + gservicesDb);
+		
+		while (!isCompletelyWritten(gservicesDb))
+		{
+			try
+			{
+				Thread.sleep(15);
+			}
+			catch (InterruptedException e)
+			{}
+		}
+		
+		gservicesDb.renameTo(gservicesWorkingDb);
+		
+		while (!isCompletelyWritten(gservicesWorkingDb))
+		{ // needs moar memes, erm, I mean time to finish writing the file
+			try
+			{
+				Thread.sleep(5);
+			}
+			catch (InterruptedException e)
+			{}
+		}
+		
+        SQLiteDatabase db = mainActivity.openOrCreateDatabase(gservicesWorkingDb.toString(), Context.MODE_WORLD_WRITEABLE, null);
         // All your overrides are belong to me.
         // If the toggle button is on already
         if (toggleButton.isChecked()) {
             // Let's disable debugging.
-            db.execSQL("DELETE FROM overrides WHERE name='" + database_name + "';");
+            db.execSQL("UPDATE overrides SET value='false' WHERE name='" + database_name + "';");
 			editor.putInt(editor_name, 0);
+			editor.commit();
         } else {
             // It's off, so we'll enable debugging.
             try {
@@ -50,18 +87,37 @@ class UpdateDatabase implements View.OnClickListener {
             }
             db.execSQL("UPDATE overrides SET value='true' WHERE name='" + database_name + "';");
 			editor.putInt(editor_name, 1);
+			editor.commit();
         }
         // Just kidding. You can have it back now.
-        Shell.SU.run("cp /data/data/com.r3pwn.LetMeMakeYouSomeSandwiches/databases/gservices.db /data/data/com.google.android.gsf/databases/gservices.db\n");
+        Shell.SU.run("cp " + gservicesWorkingDb + "/data/data/com.google.android.gsf/databases/gservices.db\n");
         Shell.SU.run("rm -f /data/data/com.r3pwn.LetMeMakeYouSomeSandwiches/databases/*\n");
         // Here in Android land, we call the following "reloading".
         Shell.SU.run("am force-stop com.google.android.gsf\n");
         Shell.SU.run("am force-stop " + app_name + "\n");
-		
-		editor.commit();
 
         // Re-enable buttons
         mainActivity.enableAll();
         Toast.makeText(mainActivity.getApplicationContext(), "Changes applied.", Toast.LENGTH_SHORT).show();
     }
+	
+	private boolean isCompletelyWritten(File file) {
+		// this was taken from http://stackoverflow.com/a/11242648
+		RandomAccessFile stream = null;
+		try {
+			stream = new RandomAccessFile(file, "rw");
+			return true;
+		} catch (Exception e) {
+			Log.d("INFO", "Skipping file " + file.getName() + " for this iteration due to it not being completely written.");
+		} finally {
+			if (stream != null) {
+				try {
+					stream.close();
+				} catch (IOException e) {
+					Log.d("INFO", "Exception during closing file " + file.getName());
+				}
+			}
+		}
+		return false;
+	}
 }
